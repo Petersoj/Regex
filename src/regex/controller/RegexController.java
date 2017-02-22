682e53995aadb4718e29df12f61f11b63344b335
Petersoj <petersojj@gmail.com>
2017-02-22 13:56:36 -0700

    Added valid

--- src/regex/controller/RegexController.java
+++ src/regex/controller/RegexController.java
@@ -1,8 +1,14 @@
 package regex.controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import regex.view.RegexFrame;
 
 public class RegexController {
+	
+	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = 
+		    Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 	
 	private RegexFrame regexFrame;
 	
@@ -12,6 +18,32 @@ public class RegexController {
 	
 	public void start(){
 		
+	}
+	
+	public boolean isValidFirstName(String firstName){
+		return true;
+	}
+	
+	public boolean isValidLastName(String lastName){
+		return true;
+	}
+	
+	public boolean isValidPhoneNumber(String phoneNumber){
+		if (phoneNumber.matches("\\d{10}")){
+			return true;
+		}else if(phoneNumber.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}")){
+			return true;
+		}else if(phoneNumber.matches("\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}")){
+			return true;
+		}else if(phoneNumber.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}")){
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isValidEmail(String email){
+		Matcher regex = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE).matcher(email);
+		return regex.find();
 	}
 	
 	public RegexFrame getRegexFrame(){
--- src/regex/view/RegexPanel.java
+++ src/regex/view/RegexPanel.java
@@ -1,13 +1,22 @@
 package regex.view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+import regex.controller.RegexController;
 
 public class RegexPanel extends JPanel {
 	
 	private RegexFrame regexFrame;
+	
+	private SpringLayout springLayout;
 	
 	private JLabel firstNameLabel;
 	private JLabel lastNameLabel;
@@ -21,12 +30,15 @@ public class RegexPanel extends JPanel {
 	
 	public RegexPanel(RegexFrame regexFrame){
 		super();
+		
 		this.regexFrame = regexFrame;
 		
-		this.firstNameLabel = new JLabel("First Name");
-		this.lastNameLabel = new JLabel("Last Name");
-		this.phoneNumberLabel = new JLabel("Phone Number");
-		this.emailLabel = new JLabel("Email");
+		this.springLayout = new SpringLayout();
+		
+		this.firstNameLabel = new JLabel("First Name:");
+		this.lastNameLabel = new JLabel("Last Name:");
+		this.phoneNumberLabel = new JLabel("Phone Number:");
+		this.emailLabel = new JLabel("Email:");
 		
 		this.firstNameField = new JTextField();
 		this.lastNameField = new JTextField();
@@ -41,15 +53,70 @@ public class RegexPanel extends JPanel {
 	}
 	
 	private void setupPanel(){
-		
+		this.setLayout(springLayout);
+		this.add(firstNameLabel);
+		this.add(lastNameLabel);
+		this.add(phoneNumberLabel);
+		this.add(emailLabel);
+		this.add(firstNameField);
+		this.add(lastNameField);
+		this.add(phoneNumberField);
+		this.add(emailField);
+		this.add(verifyButton);
 	}
 	
 	private void setupLayout(){
+		springLayout.putConstraint(SpringLayout.NORTH, firstNameLabel, 30, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, firstNameLabel, 30, SpringLayout.WEST, this);
 		
+		springLayout.putConstraint(SpringLayout.WEST, firstNameField, 30, SpringLayout.EAST, firstNameLabel);
+		springLayout.putConstraint(SpringLayout.EAST, firstNameField, -30, SpringLayout.EAST, this);
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, firstNameField, 0, SpringLayout.VERTICAL_CENTER, firstNameLabel);
+		
+		springLayout.putConstraint(SpringLayout.NORTH, lastNameLabel, 30, SpringLayout.SOUTH, firstNameLabel);
+		springLayout.putConstraint(SpringLayout.WEST, lastNameLabel, 0, SpringLayout.WEST, firstNameLabel);
+		
+		springLayout.putConstraint(SpringLayout.WEST, lastNameField, 0, SpringLayout.WEST, firstNameField);
+		springLayout.putConstraint(SpringLayout.EAST, lastNameField, 0, SpringLayout.EAST, firstNameField);
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, lastNameField, 0, SpringLayout.VERTICAL_CENTER, lastNameLabel);
+		
+		springLayout.putConstraint(SpringLayout.NORTH, phoneNumberLabel, 30, SpringLayout.SOUTH, lastNameLabel);
+		springLayout.putConstraint(SpringLayout.WEST, phoneNumberLabel, 0, SpringLayout.WEST, lastNameLabel);
+		
+		springLayout.putConstraint(SpringLayout.WEST, phoneNumberField, 0, SpringLayout.WEST, firstNameField);
+		springLayout.putConstraint(SpringLayout.EAST, phoneNumberField, 0, SpringLayout.EAST, firstNameField);
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, phoneNumberField, 0, SpringLayout.VERTICAL_CENTER, phoneNumberLabel);
+		
+		springLayout.putConstraint(SpringLayout.NORTH, emailLabel, 30, SpringLayout.SOUTH, phoneNumberLabel);
+		springLayout.putConstraint(SpringLayout.WEST, emailLabel, 0, SpringLayout.WEST, phoneNumberLabel);
+		
+		springLayout.putConstraint(SpringLayout.WEST, emailField, 0, SpringLayout.WEST, firstNameField);
+		springLayout.putConstraint(SpringLayout.EAST, emailField, 0, SpringLayout.EAST, firstNameField);
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, emailField, 0, SpringLayout.VERTICAL_CENTER, emailLabel);
+		
+		springLayout.putConstraint(SpringLayout.NORTH, verifyButton, 30, SpringLayout.SOUTH, emailLabel);
+		springLayout.putConstraint(SpringLayout.HORIZONTAL_CENTER, verifyButton, 0, SpringLayout.HORIZONTAL_CENTER, this);
 	}
 	
 	private void setupListeners(){
-		
+		this.verifyButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				RegexController regexController = regexFrame.getRegexController();
+				
+				boolean matchedFirstName = regexController.isValidFirstName(firstNameField.getText());
+				boolean matchedLastName = regexController.isValidLastName(lastNameField.getText());
+				boolean matchedPhoneNumber = regexController.isValidPhoneNumber(phoneNumberField.getText());
+				boolean matchedEmail = regexController.isValidPhoneNumber(emailField.getText());
+				
+				String shownString = "First Name: " + matchedFirstName + "\n";
+				shownString += "Last Name: " + matchedLastName + "\n";
+				shownString += "Phone Number: " + matchedPhoneNumber + "\n";
+				shownString += "Email: " + matchedEmail;
+				
+				JOptionPane.showMessageDialog(regexFrame, shownString);
+			}
+		});
 	}
 
 }
